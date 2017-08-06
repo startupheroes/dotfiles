2fcf3819cfb2809e4a07240d0c60772cc5732346
Rémi Gérard-Marchant <hello@remitbri.com>
2017-08-07 01:12:43 +0200

    Remove heuristic diff setting now used as default
    https://raw.githubusercontent.com/git/git/master/Documentation/RelNotes/2.14.0.txt
    
    Closes #797.

---
+++ extra-brew.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+brew install kubernetes-cli
+brew install terraform
+brew install mysql
+brew install go
+brew install maven
+brew install h2
---
+++ extra-cask.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+brew cask install android-studio
+brew cask install datagrip
+brew cask install dropbox
+brew cask install firefox
+brew cask install flycut
+brew cask install gogland
+brew cask install google-chrome
+brew cask install intellij-idea
+brew cask install iterm2
+brew cask installjava
+brew cask install mat
+brew cask install skype
+brew cask install slack
+brew cask install spotify
+brew cask install sublime-text
+brew cask install tunnelblick
+brew cask install utorrent
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
+brew cask install whatsapp
